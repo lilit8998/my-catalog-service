cd276cca15765e50daf521c00890866e4c0cea2d
Lilit Poghosyan <poghosyanlilit544@gmail.com>
2024-04-11 17:16:32 +0400

    Add tomcat config

---
+++ src/main/java/com/example/domain/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.domain;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found");
+    }
+}
