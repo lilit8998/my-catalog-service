6218b3c1b7a294e70f8d8aebaa3900719e5173a7
Lilit Poghosyan <poghosyanlilit544@gmail.com>
2024-04-11 17:06:37 +0400

    Initial commit

---
+++ src/main/java/com/example/MyCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCatalogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mycatalogservice/MyCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
